7b27cb839a7172d49b377ab27b709843825d593f
Matekuor <matekuor94@gmail.com>
2025-10-15 23:10:51 +0000

    Added env

--- bin/reminder-cdk.ts
+++ bin/reminder-cdk.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import "dotenv/config";
 import * as cdk from "aws-cdk-lib";
 import { ReminderCdkStack } from "../lib/reminder-cdk-stack";
 
@@ -9,7 +10,10 @@ new ReminderCdkStack(app, "ReminderCdkStack", {
    * but a single synthesized template can be deployed anywhere. */
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
